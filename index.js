02a8f307d0602a6fe35d1ebcfcab621a2b8d982d
Sleaf <shu.huang@mail.hypers.com>
2019-01-25 18:46:04 +0800

    调整结构，增加注释

--- index.js
+++ index.js
@@ -2,16 +2,23 @@ const cluster = require('cluster');
 const { processNum, parallelNum } = require('./config');
 /*
 * 通讯结构:
+* master => worker
 * {
-*   code: 0(finished) / 200(normal) / 400(error)
+*   code: 0(no task) / 200(normal)
+*   payload: <Any>
+* }
+*
+* worker => master
+* {
+*   code: 0(finished) / 100(query task) / 200(normal) / 400(error)
 *   payload: <Any>
 * }
 * */
 
 if (cluster.isMaster) {
   /*
-  * Main process
-  * code will exec only once
+  * Master process
+  * codes will exec only once
   * */
   const tasks = require('./custom/tasks');
   const { every, all } = require('./custom/callback');
@@ -33,8 +40,10 @@ if (cluster.isMaster) {
       }
     }
   };
-  const onMessage = worker => ({ code, payload }) => {
+  const onTaskFinished = worker => ({ code, payload }) => {
     switch (code) {
+      case 100:
+        return askForTask(worker);
       case 200:
         //worked data
         const mappedPayload = every(payload);
@@ -50,13 +59,15 @@ if (cluster.isMaster) {
   // fork workers.
   for (let i = 0; i < processNum; i++) {
     const worker = cluster.fork();
-    worker.on('message', onMessage(worker));
-    askForTask(worker);
+    worker.on('message', onTaskFinished(worker));
   }
 } else {
-  //worker actions
+  /*
+  * Worker process
+  * codes will run many times!
+  * */
   const reducer = require('./custom/reducer');
-  process.on('message', ({ code, payload }) => {
+  const onGetNewTask = ({ code, payload }) => {
     switch (code) {
       case 200:
         return reducer(payload).then(
@@ -64,7 +75,9 @@ if (cluster.isMaster) {
           err => process.send({ code: 400, payload: err })
         );
       case 0:
-        return process.disconnect();
+      //wait for rest task and disconnect after all done.
     }
-  });
+  };
+  process.send({ code: 100, payload: null });//get first task
+  process.on('message', onGetNewTask);
 }
